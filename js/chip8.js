f9a97d637877f0abbd2ed36d49ed3700e4382527
Adil Lari <adillari@pm.me>
2024-05-12 18:48:01 -0500

    initialize keyboard/speaker

--- js/chip8.js
+++ js/chip8.js
@@ -1,7 +1,7 @@
 import Screen from './screen.js';
+import Keyboard from './keyboard.js';
+import Speaker from './speaker.js'
 
 const screen = new Screen(15);
-
-screen.togglePixel(0, 0);
-screen.togglePixel(5, 2);
-screen.refresh();
+const keyboard = new Keyboard;
+const speaker = new Speaker();
