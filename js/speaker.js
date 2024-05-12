dd6d6be925a50efbef90db4124be5901bb7b047c
Adil Lari <adillari@pm.me>
2024-05-12 18:47:46 -0500

    Speaker

---
+++ js/speaker.js
@@ -0,0 +1,28 @@
+class Speaker {
+  constructor() {
+    this.audioContext = new window.AudioContext;
+    this.gain = this.audioContext.createGain();
+    this.finish = this.audioContext.destination;
+    this.gain.connect(this.finish);
+  }
+
+  play(frequency) {
+    if (this.audioContext && !this.oscillator) {
+      this.oscillator = this.audioContext.createOscillator();
+      this.oscillator.frequency.setValueAtTime(frequency || 440, this.audioCtx.currentTime);
+      this.oscillator.type = "square";
+      this.oscillator.connect(this.gain);
+      this.oscillator.start();
+    }
+  }
+
+  stop() {
+    if (this.oscillator) {
+      this.oscillator.stop();
+      this.oscillator.disconnect();
+      this.oscillator = null;
+    }
+  }
+}
+
+export default Speaker;
