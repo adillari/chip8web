015d163694b66ebce125d72a8627409015ff5b70
Adil Lari <adillari@pm.me>
2024-05-25 23:40:35 -0500

    implement CLS and RET

--- js/cpu.js
+++ js/cpu.js
@@ -92,6 +92,43 @@ class CPU {
     
     switch(opcode & 0xF000) {
       case 0x0000:
+        switch(opcode) {
+          case 0x00E0: // CLS
+            this.SCREEN.clear();
+            break;
+          case: 0x00EE: // RET
+            this.PC = this.STACK.pop();
+            break;
+        break;
+      case 0x1000:
+        break;
+      case 0x2000:
+        break;
+      case 0x3000:
+        break;
+      case 0x4000:
+        break;
+      case 0x5000:
+        break;
+      case 0x6000:
+        break;
+      case 0x7000:
+        break;
+      case 0x8000:
+        break;
+      case 0x9000:
+        break;
+      case 0xA000:
+        break;
+      case 0xB000:
+        break;
+      case 0xC000:
+        break;
+      case 0xD000:
+        break;
+      case 0xE000:
+        break;
+      case 0xF000:
         break;
       default:
         throw new Error('Unknown opcode ' + opcode);
