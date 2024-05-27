b4ea8f8d4a991234f4f42779f2e832e28671a174
Adil Lari <adillari@pm.me>
2024-05-26 21:05:52 -0500

    create startMachine()

--- js/chip8.js
+++ js/chip8.js
@@ -6,4 +6,25 @@ import CPU from './cpu.js'
 const screen = new Screen(15);
 const keyboard = new Keyboard;
 const speaker = new Speaker();
-const cpu = new CPU();
+const cpu = new CPU(screen, keyboard, speaker);
+
+let framerate = 1000/60;
+let then = Date.now();
+let now, elapsed;
+
+function step() {
+  now = Date.now();
+  elapsed = now - then;
+  if (elapsed > framerate) {
+    cpu.cycle();
+    then = now;
+  }
+  requestAnimationFrame(step);
+}
+
+function startMachine(rom) {
+  cpu.loadHexSpritesIntoRAM();
+  cpu.loadRom(rom);
+  requestAnimationFrame(step);
+}
+
