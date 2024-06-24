ed7677ac9432f649a85cbe64e5816be5b390f536
Adil Lari <adillari@pm.me>
2024-06-24 17:29:27 -0500

    lint

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,10 @@ const port: Number = 7000;
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.use(express.static('public/'));
+app.use(express.static("public/"));
 
 app.get("/", (req: Request, res: Response) => {
-  const roms = readdirSync("public/roms/")
+  const roms = readdirSync("public/roms/");
   res.render("index", { roms });
 });
 
