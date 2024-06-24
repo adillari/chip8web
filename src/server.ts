a26d7c2d1aa441db55563a6bda85bde084337d41
Adil Lari <adillari@pm.me>
2024-06-24 17:24:11 -0500

    big reorganization

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import { readdirSync } from "fs";
+
+const app: Application = express();
+const port: Number = 7000;
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use(express.static('public/'));
+
+app.get("/", (req: Request, res: Response) => {
+  const roms = readdirSync("public/roms/")
+  res.render("index", { roms });
+});
+
+app.listen(port);
